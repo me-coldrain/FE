0e33d0de31ae289180cf99d104919c7d8509c507
gwangbaekun <gwangbaekun@gmail.com>
2022-07-26 15:59:31 +0900

    [CHORE] deploy with https

--- src/pages/team/[teamName]/members.tsx
+++ src/pages/team/[teamName]/members.tsx
@@ -7,18 +7,18 @@ const MembersList = ({ data }: any): JSX.Element => {
   return <Members data={data} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const teamId = context.query.teamId as string;
-  console.log(teamId);
-  const data = await makeRequest({
-    endpoint: `home/teams/${teamId}/players`,
-    method: "GET",
-    auth: true,
-    token: context.req.cookies.token,
-  });
-  return {
-    props: { data: data },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const teamId = context.query.teamId as string;
+//   console.log(teamId);
+//   const data = await makeRequest({
+//     endpoint: `home/teams/${teamId}/players`,
+//     method: "GET",
+//     auth: true,
+//     token: context.req.cookies.token,
+//   });
+//   return {
+//     props: { data: data },
+//   };
+// };
 
 export default MembersList;
